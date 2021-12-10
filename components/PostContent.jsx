e254ff2f59f4d352a50257a7110fce11d4ca17ce
Kerberos9 <mll.kerberos@gmail.com>
2021-12-10 20:09:01 +0100

    Updated post links

--- components/PostContent.jsx
+++ components/PostContent.jsx
@@ -12,7 +12,7 @@ export default function PostContent({ post }) {
             <h1>{post?.title}</h1>
             <span className='text-sm'>
                 Written by{' '}
-                <Link href={`/$post.username}/`}>
+                <Link href={`/${post.username}/`}>
                     <a className='text-info'>@{post.username}</a>
                 </Link>{' '}
                 on {createdAt.toISOString()}
